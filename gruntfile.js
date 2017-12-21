6c5c179fcae041750589f726cf2a271fbe6ddfeb
Margo van de Pas <margo@MacBook-Pro-van-Margo.local>
2017-12-21 10:32:59 +0100

    grunt

---
+++ gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            minify: {
+                src: 'stylesheet.css',
+                dest: 'style-min.css'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            build: {
+                src: '*.js',
+                dest: 'build/js/<%= pkg.name %>.min.js'
+            }
+        },
+
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['env']
+            },
+            dist: {
+                files: {
+                    'dist/app.js': 'script.js'
+                }
+            }
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.registerTask('default', ['babel']);
+
+
+};
